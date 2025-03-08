1742852e500bb649c9fa68f7b21e37d5c4142149
HieuDo <dotrunghieu0903@gmail.com>
2025-03-07 22:06:45 -0500

    Added Category of VOC

--- rtdetr_pytorch/src/data/voc/voc_dataset.py
+++ rtdetr_pytorch/src/data/voc/voc_dataset.py
@@ -52,3 +52,29 @@ class VOC2012Detection(VOCDetection):
 
     def extra_repr(self) -> str:
         return f' img_folder: {self.root}\n year: 2012\n'
+
+voc2012_category2name = {
+    1: "aeroplane",
+    2: "bicycle",
+    3: "bird",
+    4: "boat",
+    5: "bottle",
+    6: "bus",
+    7: "car",
+    8: "cat",
+    9: "chair",
+    10: "cow",
+    11: "diningtable",
+    12: "dog",
+    13: "horse",
+    14: "motorbike",
+    15: "person",
+    16: "pottedplan",
+    17: "sheep",
+    18: "sofa",
+    19: "train",
+    20: "tvmonitor"
+}
+
+voc_category2label = {k: i for i, k in enumerate(voc2012_category2name.keys())}
+voc_label2category
