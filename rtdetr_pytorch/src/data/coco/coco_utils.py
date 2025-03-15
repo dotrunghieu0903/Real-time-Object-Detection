a738ab1f61e26369d014b9ec51c8ddec784e56ab
HieuDo <dotrunghieu0903@gmail.com>
2025-03-15 00:54:23 -0400

    Updated COCO

--- rtdetr_pytorch/src/data/coco/coco_utils.py
+++ rtdetr_pytorch/src/data/coco/coco_utils.py
@@ -118,56 +118,6 @@ def _coco_remove_images_without_annotations(dataset, cat_list=None):
     return dataset
 
 
-# def convert_to_coco_api(ds):
-#     coco_ds = COCO()
-#     # annotation IDs need to start at 1, not 0, see torchvision issue #1530
-#     ann_id = 1
-#     dataset = {"images": [], "categories": [], "annotations": []}
-#     categories = set()
-#     for img_idx in range(len(ds)):
-#         # find better way to get target
-#         # targets = ds.get_annotations(img_idx)
-#         img, targets = ds[img_idx]
-#         image_id = targets["image_id"].item()
-#         img_dict = {}
-#         img_dict["id"] = image_id
-#         img_dict["height"] = img.shape[-2]
-#         img_dict["width"] = img.shape[-1]
-#         dataset["images"].append(img_dict)
-#         bboxes = targets["boxes"].clone()
-#         bboxes[:, 2:] -= bboxes[:, :2]
-#         bboxes = bboxes.tolist()
-#         labels = targets["labels"].tolist()
-#         areas = targets["area"].tolist()
-#         iscrowd = targets["iscrowd"].tolist()
-#         if "masks" in targets:
-#             masks = targets["masks"]
-#             # make masks Fortran contiguous for coco_mask
-#             masks = masks.permute(0, 2, 1).contiguous().permute(0, 2, 1)
-#         if "keypoints" in targets:
-#             keypoints = targets["keypoints"]
-#             keypoints = keypoints.reshape(keypoints.shape[0], -1).tolist()
-#         num_objs = len(bboxes)
-#         for i in range(num_objs):
-#             ann = {}
-#             ann["image_id"] = image_id
-#             ann["bbox"] = bboxes[i]
-#             ann["category_id"] = labels[i]
-#             categories.add(labels[i])
-#             ann["area"] = areas[i]
-#             ann["iscrowd"] = iscrowd[i]
-#             ann["id"] = ann_id
-#             if "masks" in targets:
-#                 ann["segmentation"] = coco_mask.encode(masks[i].numpy())
-#             if "keypoints" in targets:
-#                 ann["keypoints"] = keypoints[i]
-#                 ann["num_keypoints"] = sum(k != 0 for k in keypoints[i][2::3])
-#             dataset["annotations"].append(ann)
-#             ann_id += 1
-#     dataset["categories"] = [{"id": i} for i in sorted(categories)]
-#     coco_ds.dataset = dataset
-#     coco_ds.createIndex()
-#     return coco_ds
 def convert_to_coco_api(ds):
     coco_ds = COCO()
     # annotation IDs need to start at 1, not 0, see torchvision issue #1530
@@ -178,61 +128,112 @@ def convert_to_coco_api(ds):
         # find better way to get target
         # targets = ds.get_annotations(img_idx)
         img, targets = ds[img_idx]
-        image_id = img_idx  # Assuming image_id is the index of the image
+        image_id = targets["image_id"].item()
         img_dict = {}
         img_dict["id"] = image_id
         img_dict["height"] = img.shape[-2]
         img_dict["width"] = img.shape[-1]
         dataset["images"].append(img_dict)
-        
-        # Assuming targets is a tuple of (boxes, labels)
-        boxes, labels = targets
-        
-        bboxes = boxes.clone()
+        bboxes = targets["boxes"].clone()
+        bboxes[:, 2:] -= bboxes[:, :2]
         bboxes = bboxes.tolist()
-        
-        # Get the labels from the dictionary
-        label_ids = labels['labels'].tolist()
-        
+        labels = targets["labels"].tolist()
+        areas = targets["area"].tolist()
+        iscrowd = targets["iscrowd"].tolist()
+        if "masks" in targets:
+            masks = targets["masks"]
+            # make masks Fortran contiguous for coco_mask
+            masks = masks.permute(0, 2, 1).contiguous().permute(0, 2, 1)
+        if "keypoints" in targets:
+            keypoints = targets["keypoints"]
+            keypoints = keypoints.reshape(keypoints.shape[0], -1).tolist()
         num_objs = len(bboxes)
         for i in range(num_objs):
             ann = {}
             ann["image_id"] = image_id
-            ann["bbox"] = [bboxes[i][0][0], bboxes[i][0][1], bboxes[i][0][2], bboxes[i][0][3]]
-            ann["category_id"] = label_ids[i]
-            categories.add(label_ids[i])
-            ann["area"] = (bboxes[i][0][2] - bboxes[i][0][0]) * (bboxes[i][0][3] - bboxes[i][0][1])
-            ann["iscrowd"] = 0
+            ann["bbox"] = bboxes[i]
+            ann["category_id"] = labels[i]
+            categories.add(labels[i])
+            ann["area"] = areas[i]
+            ann["iscrowd"] = iscrowd[i]
             ann["id"] = ann_id
+            if "masks" in targets:
+                ann["segmentation"] = coco_mask.encode(masks[i].numpy())
+            if "keypoints" in targets:
+                ann["keypoints"] = keypoints[i]
+                ann["num_keypoints"] = sum(k != 0 for k in keypoints[i][2::3])
             dataset["annotations"].append(ann)
             ann_id += 1
-    # Update categories to match Pascal VOC 2007 classes
-    categories = {
-        1: "aeroplane",
-        2: "bicycle",
-        3: "bird",
-        4: "boat",
-        5: "bottle",
-        6: "bus",
-        7: "car",
-        8: "cat",
-        9: "chair",
-        10: "cow",
-        11: "diningtable",
-        12: "dog",
-        13: "horse",
-        14: "motorbike",
-        15: "person",
-        16: "pottedplant",
-        17: "sheep",
-        18: "sofa",
-        19: "train",
-        20: "tvmonitor"
-    }
-    dataset["categories"] = [{"id": i, "name": categories[i]} for i in sorted(categories.keys())]
+    dataset["categories"] = [{"id": i} for i in sorted(categories)]
     coco_ds.dataset = dataset
     coco_ds.createIndex()
     return coco_ds
+
+# def convert_to_voc_api(ds):
+#     voc_ds = COCO()
+#     # annotation IDs need to start at 1, not 0, see torchvision issue #1530
+#     ann_id = 1
+#     dataset = {"images": [], "categories": [], "annotations": []}
+#     categories = set()
+#     for img_idx in range(len(ds)):
+#         # find better way to get target
+#         # targets = ds.get_annotations(img_idx)
+#         img, targets = ds[img_idx]
+#         image_id = img_idx  # Assuming image_id is the index of the image
+#         img_dict = {}
+#         img_dict["id"] = image_id
+#         img_dict["height"] = img.shape[-2]
+#         img_dict["width"] = img.shape[-1]
+#         dataset["images"].append(img_dict)
+        
+#         # Assuming targets is a tuple of (boxes, labels)
+#         boxes, labels = targets
+        
+#         bboxes = boxes.clone()
+#         bboxes = bboxes.tolist()
+        
+#         # Get the labels from the dictionary
+#         label_ids = labels['labels'].tolist()
+        
+#         num_objs = len(bboxes)
+#         for i in range(num_objs):
+#             ann = {}
+#             ann["image_id"] = image_id
+#             ann["bbox"] = [bboxes[i][0][0], bboxes[i][0][1], bboxes[i][0][2], bboxes[i][0][3]]
+#             ann["category_id"] = label_ids[i]
+#             categories.add(label_ids[i])
+#             ann["area"] = (bboxes[i][0][2] - bboxes[i][0][0]) * (bboxes[i][0][3] - bboxes[i][0][1])
+#             ann["iscrowd"] = 0
+#             ann["id"] = ann_id
+#             dataset["annotations"].append(ann)
+#             ann_id += 1
+#     # Update categories to match Pascal VOC 2007 classes
+#     categories = {
+#         1: "aeroplane",
+#         2: "bicycle",
+#         3: "bird",
+#         4: "boat",
+#         5: "bottle",
+#         6: "bus",
+#         7: "car",
+#         8: "cat",
+#         9: "chair",
+#         10: "cow",
+#         11: "diningtable",
+#         12: "dog",
+#         13: "horse",
+#         14: "motorbike",
+#         15: "person",
+#         16: "pottedplant",
+#         17: "sheep",
+#         18: "sofa",
+#         19: "train",
+#         20: "tvmonitor"
+#     }
+#     dataset["categories"] = [{"id": i, "name": categories[i]} for i in sorted(categories.keys())]
+#     coco_ds.dataset = dataset
+#     coco_ds.createIndex()
+#     return coco_ds
 
 def get_coco_api_from_dataset(dataset):
     # FIXME: This is... awful?
